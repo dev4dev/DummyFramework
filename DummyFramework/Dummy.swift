6a65d411c6784f19e1f5fb7bcd98652d32e047e7
Alex Antonyuk <alex@antonyuk.me>
2017-11-07 13:32:04 +0200

    impl

---
+++ DummyFramework/Dummy.swift
@@ -0,0 +1,15 @@
+//
+//  Dummy.swift
+//  DummyFramework
+//
+//  Created by Alex Antonyuk on 11/7/17.
+//  Copyright © 2017 Alex Antonyuk. All rights reserved.
+//
+
+import Foundation
+
+final public class Dummy {
+	public func what() -> String {
+		return "💩"
+	}
+}
