9b2782890af77f9278a5d161cc3463dee554fb5c
Alex Antonyuk <alex@antonyuk.me>
2017-11-07 13:24:40 +0200

    Initial Commit

---
+++ DummyFramework/DummyFramework.h
@@ -0,0 +1,19 @@
+//
+//  DummyFramework.h
+//  DummyFramework
+//
+//  Created by Alex Antonyuk on 11/7/17.
+//  Copyright © 2017 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DummyFramework.
+FOUNDATION_EXPORT double DummyFrameworkVersionNumber;
+
+//! Project version string for DummyFramework.
+FOUNDATION_EXPORT const unsigned char DummyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyFramework/PublicHeader.h>
+
+
